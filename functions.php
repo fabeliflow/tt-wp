54d38220639f273f994b801622638bdb44dc1db5
fabeliflow <fabeliflow@gmail.com>
2019-06-12 18:04:16 -0400

    Updated card for better readability.

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function custom_field_excerpt($field) {
     $text = strip_shortcodes( $text );
     $text = apply_filters('the_content', $text);
     $text = str_replace(']]>', ']]>', $text);
-    $excerpt_length = apply_filters('excerpt_length', 40);
+    $excerpt_length = apply_filters('excerpt_length', 100);
     return wp_trim_words( $text, $excerpt_length );
 }
 
@@ -55,7 +55,7 @@ add_filter( 'alm_query_args_searchwp', 'my_alm_query_args_searchwp');
 function wpb_adding_styles_scripts() {
 	
 	// general styles
-	wp_register_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,400i,700%7CNews+Cycle:400,700%7CCarter+One' );
+	wp_register_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,400i,700%7CNews+Cycle:400,700' );
 
 	wp_register_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css' );
 
