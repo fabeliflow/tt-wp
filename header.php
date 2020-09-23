1bfcd09910d1a3a79fb11065f11b62c1dbffded9
fabeliflow <fabeliflow@gmail.com>
2020-09-23 12:02:13 -0400

    Removed extra meta description tag.

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-    <meta name="description" content="<?php bloginfo('description'); ?>">
 
     <title>
         <?php wp_title(''); ?>
