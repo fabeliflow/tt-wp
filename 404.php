8b09ba227eae09b07e29c18e22d3e862d4ee8b70
fabeliflow <fabeliflow@gmail.com>
2021-05-21 15:02:04 -0400

    Fixed 404 styles.

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ Template Name: 404
                 <div class="col col-lg-6 tt-404__header">
                     <h1><?php the_field('404_header'); ?></h1>
                     <span><?php the_field('404_subheader'); ?></span>
-                    <a class="tt-btn tt-btn--ghost" href="<?php the_field('404_button_url'); ?>"><?php the_field('404_button_text'); ?>
+                    <a class="tt-btn tt-btn--ghost tt-btn--ghost-white" href="<?php the_field('404_button_url'); ?>"><?php the_field('404_button_text'); ?>
                     </a>
                 </div>
             </div>
