3c0bb7f7c792da169695f0dbb0e0982b3f2cc062
fabeliflow <fabeliflow@gmail.com>
2021-06-09 10:14:32 -0400

    Removed unnecessary favicon script.

--- header.php
+++ header.php
@@ -14,14 +14,6 @@
         <?php bloginfo('name'); ?>
     </title>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#dbcaac">
-    <meta name="msapplication-TileColor" content="#100e17">
-    <meta name="theme-color" content="#100e17">
-
     <?php wp_head(); ?>
 
 </head>
